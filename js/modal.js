50afea9916d60ebabe35334e0c5f11df57c7d682
aditi181920 <pheonackpizzep@gmail.com>
2022-10-25 23:27:12 +0530

    prescription page final

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+function fun(modalid,buttonid,closeid){
+    var modal = document.getElementById(modalid);
+    var btn = document.getElementById(buttonid);
+    var span = document.getElementById(closeid);
+    modal.style.display = "block";
+    span.onclick = function() {
+        modal.style.display = "none";
+      }
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
+}
+
+
+
