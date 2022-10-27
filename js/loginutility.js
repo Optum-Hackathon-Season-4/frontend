e67b3edd20a0d4135f7f45d33cb80c2c0b153d0b
dragonman164 <sanidhiyafirefox123@gmail.com>
2022-10-27 22:53:43 +0530

    integration started

--- js/loginutility.js
+++ js/loginutility.js
@@ -17,3 +17,113 @@ function pagehandler(value,idd){
   }
   document.getElementById(idd).selectedIndex=-1;
 }
+
+
+
+function Login(){
+  let username = document.getElementById("username").value
+  let password = document.getElementById("pass").value
+
+  if(username == "" || password == ""){
+    document.getElementById("message1").innerHTML = "Details Missing"
+  }else{
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "username": username,
+  "password": password
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:8000/login/", requestOptions)
+  .then(response => response.text())
+  .then((result)=>{
+    let data = JSON.parse(result)
+    localStorage.clear()
+    localStorage.setItem('token',data.token)
+    localStorage.setItem('patient',true)
+    window.location.replace("home.html")
+  })
+  .catch((error)=>{
+    console.log(error)
+    document.getElementById("message1").innerHTML = "Login Failed"
+
+  });
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function submitDetails(){
+  let name = document.getElementById("uname").value
+  let username = document.getElementById("user").value
+  let birthdate = document.getElementById("birthday").value
+  let marital = document.getElementById("martialstat").value
+  let gender = document.getElementById("gender").value
+  let address = document.getElementById("address").value
+  let contact = document.getElementById("contact").value
+  let password = document.getElementById("password").value
+
+  if(name == "" || username == "" || birthdate == "" || marital == "" || gender == "" 
+|| address  == "" || contact == "" || password == "")
+{
+  document.getElementById("message").innerHTML = "Please Check if all Details are Valid"
+}else{
+  var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "password": password,
+  "username": username,
+  "is_patient": true,
+  "name": name,
+  "gender": gender,
+  "birth_date": birthdate,
+  "marital_status": marital,
+  "address": address,
+  "phone_number": contact
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:8000/signup/", requestOptions)
+  .then(response => response.text())
+  .then((result)=>{
+    document.getElementById("message").style = "color: green;"
+    document.getElementById("message").innerHTML = "User Succesfully Registered!!"
+
+  })
+  .catch((error)=>{
+    document.getElementById("message").innerHTML = "There was some error please try later"
+
+  });
+}
+
+
+
+  
+  
+
+  
+}
--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,68 @@
+var myHeaders = new Headers();
+myHeaders.append("Authorization", `Token ${localStorage.getItem('token')}`);
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("http://localhost:8000/prescriptions/", requestOptions)
+  .then((response) => response.text())
+  .then((result) => {
+    let data = JSON.parse(result)
+    data.forEach((elem) => {
+        const e1 = document.createElement(`
+        <div class="split right">
+        <div class="card">
+            <div class="leftcard">
+                <div class="dateissue">
+                    <p>25 October<br>2022</p>
+                </div>
+            </div>
+            <div class="midcard">
+                <div>
+                    <p>Prescription id: ${elem.id}</p>
+                </div>
+                <div>
+                    <p>Date to be paid off: ${elem.payment_deadline === null?'N/A' : elem.payment_deadline}</p>
+                </div>
+                <div>
+                    <p>Symptomps : ${elem.symptoms}</p>
+                </div>
+                <div>
+                    <p>Follow Up : ${elem.follow_up}</p>
+                </div>
+                <div>
+                    <p>No. of Days : ${elem.days}</p>
+                </div>
+            </div>
+            <div class="rightcard">
+                <button onclick="fun('myModal1','buttn1','close1')"id="buttn1">
+                    Details
+                </button>
+            </div>
+            <div id="myModal1" class="modal">
+                <div class="modal-content">
+                  <span id="close1">&times;</span>
+                  <table class="tabb">
+                    <tr><th>Medicine</th><th>Dosage</th><th>Cost</th></tr>
+                    <tr><td>Paracetamol 50 mg</td><td>1 time a day in morning for 1 week</td><td>$12</td></tr>
+                    <tr><td>Paracetamol 50 mg</td><td>1 time a day in morning for 1 week</td><td>$12</td></tr>
+                    <tr><td>Paracetamol 50 mg</td><td>1 time a day in morning for 1 week</td><td>$12</td></tr>
+                  </table>
+                </div>
+              
+            </div>
+        </div>`)
+        document.getElementById("prescriptionlist").appendChild(e1)
+    });
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+
+
 function fun(modalid,buttonid,closeid){
     var modal = document.getElementById(modalid);
     var btn = document.getElementById(buttonid);
@@ -12,6 +77,8 @@ function fun(modalid,buttonid,closeid){
     }
     }
 }
+
+
 
 
 
