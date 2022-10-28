ad3140e326234bee8f861bb682a6722a56122c0f
dragonman164 <sanidhiyafirefox123@gmail.com>
2022-10-29 01:00:07 +0530

    admin 90%done

--- js/modal.js
+++ js/modal.js
@@ -99,3 +99,164 @@ function change(button){
     // console.log(`date to be paid off: ${store}`);
     button.parentElement.previousElementSibling.previousElementSibling.lastElementChild.firstElementChild.textContent=`Date to be paid off: ${(store)}`;
 }
+
+
+function fetchFeedBacks(event){
+    event.preventDefault()
+    let doctor = document.getElementById("patientid").value
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append( 'Accept', 'application/json',)
+
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch(`http://localhost:8000/openfeedback/${doctor}`, requestOptions)
+  .then(response => response.text())
+  .then((result) => {
+    let data1 = JSON.parse(result)
+    data1.forEach((elem)=>{
+        document.getElementById("feedbacks").innerHTML+= `
+        
+        <div class="rating_card">
+        <div class="topdet">
+            <p class="txt">Prescription ID : ${elem.id}<br>Patient ID : ${elem.doctor} <br> Doctor ID : ${elem.patient} <br> Approved : ${elem.approved}</p>
+        </div>
+        <div class="botdet">
+            <div class="ratings">
+                <div class="treatment_rating">
+                    <span class="iconselected">&#9733;</span>
+                    <span class="iconselected">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                </div>
+                <div class="communication_rating">
+                    <span class="iconselected">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                </div>
+                <div class="collaboration_rating">
+                    <span class="iconselected">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                </div>
+                <div class="availability">
+                    <span class="iconselected">&#9733;</span>
+                    <span class="iconselected">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                    <span class="icon">&#9733;</span>
+                </div>
+                <div class="commentfordoctor">
+                    <button class="buttoncomment" onclick="approvefeedback(${elem.id})" id="buttonapprove">Approve Feedback</button>
+                </div>
+                <div id="myModal1" class="modal">
+                    <div class="modal-content">
+                      <span id="close1">&times;</span>
+                      <p>${elem.reviews}</p>
+                    </div>
+                </div>
+             </div>
+             <div class="labelss">
+                <div class="lab">Treatment rating</div>
+                <div class="lab">Communication rating</div>
+                <div class="lab">Collaboration rating</div>
+                <div class="lab">Availability</div>
+                <div class="lab">Comment for your doctor</div>
+             </div>
+        </div>
+        <div class="decision">
+              <div class="det">
+              ${elem.reviews}
+              </div> 
+              <h5 style="color: green;" id="good${elem.id}"> </h5>
+              <h5 style="color: red;" id="$bad${elem.id}">  </h5>
+              
+              <div>
+                <button onclick="getReviews(\`${elem.reviews}\`,${elem.id})" class="buttonnlp">
+                    analyse review
+                </button>
+              </div>
+        </div>
+    </div>
+        `
+    })
+    
+})
+  .catch((error) => {
+    document.getElementById("error").innerHTML="There was some Error"
+});
+}
+
+
+function approvefeedback(id){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${localStorage.getItem('token')}`);
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      "id": 1
+    });
+    
+    var requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("http://localhost:8000/feedback/", requestOptions)
+      .then(response => response.text())
+      .then((result) => {
+
+        document.getElementById(`good${id}`).innerHTML = "Successfully Approved"
+    })
+      .catch((error) => {
+        document.getElementById(`bad${id}`).innerHTML = "There was some error"
+
+        
+      });
+}
+
+function getReviews(text,id){
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+        "text": text,
+        });
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("http://localhost:8000/reviews/", requestOptions)
+        .then(response => response.text())
+        .then((result) => {
+            let data = JSON.parse(result)
+            document.getElementById(`good${id}`).innerHTML = `Review Type:  ${data.message}`
+        })
+        .catch((error) => {
+        
+            console.log('error', error)
+        });
+}
+
+
+function adminCheck(event){
+    event.preventDefault()
+    console.log(document.getElementById("patientid1").value)
+
+}
