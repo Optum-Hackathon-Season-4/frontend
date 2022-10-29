2c61c18e80b2e74dc4dd7d0bb75f90414538edce
aditi181920 <pheonackpizzep@gmail.com>
2022-10-29 16:22:17 +0530

    completing rating system

--- js/ratinghandler.js
+++ js/ratinghandler.js
@@ -1,37 +1,4 @@
-$(':radio').change(function() {
-    console.log('New star rating: ' + this.value);
-  });
-  
+
+//console.log(document.getElementsByClassName('treatment_rating')[0].querySelector('input[name="stars"]:checked').value);
 //fetch all doctors first 
 
-  //feedback post
-
-  // var myHeaders = new Headers();
-  // myHeaders.append("Authorization", `Token ${JSON.parse(localStorage.getItem('token'))}`);
-  // myHeaders.append("Content-Type", "application/json");
-  
-  // var raw = JSON.stringify({
-  //   "doctor": 1,
-  //   "availability": 10,
-  //   "collaboration_rating": 10,
-  //   "communication_rating": 10,
-  //   "treatment_rating": 10,
-  //   "reviews": "sdadasd"
-  // });
-  
-  // var requestOptions = {
-  //   method: 'POST',
-  //   headers: myHeaders,
-  //   body: raw,
-  //   redirect: 'follow'
-  // };
-  
-  // fetch("http://localhost:8000/feedback/", requestOptions)
-  //   .then(response => response.text())
-  //   .then((result)=>{
-  //     let data = JSON.parse(result)
-  //   })
-  //   .catch((error)=>{
-  //     console.log(error)
-  //     document.getElementById("message1").innerHTML = "Login Failed"
-  //   });
