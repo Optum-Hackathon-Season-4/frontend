266cd12b2bb6a1ad2fa9d459276d9187d0b4e4e8
aditi181920 <pheonackpizzep@gmail.com>
2022-10-30 21:32:23 +0530

    completing doctor

---
+++ js/approveapoint.js
@@ -0,0 +1,24 @@
+function approveapoin(pid){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization",`Token ${localStorage.getItem('token')}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "id": pid
+    });
+
+    var requestOptions = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("http://localhost:8000/appointments/", requestOptions)
+    .then(response => response.text())
+    .then((result)=>{
+        console.log(result);
+        console.log('done');
+    })
+    .catch(error => console.log('error', error));
+}
